9645713e007074d748a748953935b62f567d7220
Michael E. Cotterell <mepcotterell@gmail.com>
2015-02-23 10:50:08 -0500

    updated skeleton code

--- src/main/java/cs1302/p2/Driver.java
+++ src/main/java/cs1302/p2/Driver.java
@@ -3,38 +3,47 @@ package cs1302.p2;
 import cs1302.effects.Artsy;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
+import javax.swing.SwingUtilities;
 
+/**
+ * This is the driver for this application. It ensures that GUI components
+ * are created in the Event Dispatch Thread.
+ */
 public class Driver {
-	
+
+    /**
+     * Creates the frame for this application and sets up the main panel.
+     */
     public static void createAndShowGUI() {
-		
+
+        // Create the frame
     	JFrame frame = new JFrame("Artsy!");
     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // The single MyArtsy object that you are allowed to pass around
         Artsy artsy = new MyArtsy();
 
-        // YOUR GUI CODE CAN START HERE
-        // I recommend that the constructor to your main panel object take in
-        // an Artsy object so that your action listeners, etc. can use it.
+        // Create the main panel and add it to the frame
+        JPanel mainPanel = new MainPanel(artsy);
+        frame.add(mainPanel);
 
     	// show the window.
         frame.pack();
         frame.setVisible(true);
 		
     } // createAndShowGUI
-	
+
+    /**
+     * The entry point into the program. 
+     *
+     * @param args  command line arguments
+     */
     public static void main(String[] args) {
-		
-	// Schedule a job for the event dispatch thread:
+        // Schedule a job for the event dispatch thread:
         // creating and showing this application's GUI.
-	javax.swing.SwingUtilities.invokeLater(new Runnable() {
-		public void run() {
-		    createAndShowGUI();
-		} // run
-	    });
-		
+        SwingUtilities.invokeLater(() -> createAndShowGUI());
     } // main
 
 } // Driver
