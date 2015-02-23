bbea9380c50f2e5891d0f145e1475d057b567665
Michael E. Cotterell <mepcotterell@gmail.com>
2015-02-23 11:04:02 -0500

    updated the interface

--- src/main/java/cs1302/effects/Artsy.java
+++ src/main/java/cs1302/effects/Artsy.java
@@ -12,9 +12,8 @@ public interface Artsy {
 	 * Given two BufferedImages, this method returns a new BufferedImage, the contents of which
 	 * is composed of the source images alternating horizontally and vertically at the specified 
 	 * size, in pixels. This causes the images to appear woven together in a checkered fashion. 
-	 * For an example, please see the reference provided in the see also section of this comment.
+	 * For an example, please see the project description.
 	 * 
-	 * @see http://tinyurl.com/amlo7w7
 	 * @param src1 the first BufferedImage
 	 * @param src2 the second BufferedImage
 	 * @param height the height, in pixels, of the horizontal stripes.
@@ -25,9 +24,8 @@ public interface Artsy {
 	/**
 	 * Given two BufferedImages, this method returns a new BufferedImage, the contents of which
 	 * is composed of the source images alternating horizontally at the specified pixel height. 
-	 * For an example, please see the reference provided in the see also section of this comment.
-	 * 
-	 * @see http://tinyurl.com/ahejkbx
+	 * For an example, please see the project description.
+     *
 	 * @param src1 the first BufferedImage
 	 * @param src2 the second BufferedImage
 	 * @param height the height, in pixels, of the horizontal stripes.
@@ -38,9 +36,8 @@ public interface Artsy {
 	/**
 	 * Given two BufferedImages, this method returns a new BufferedImage, the contents of which
 	 * is composed of the source images alternating vertically at the specified pixel width. For
-	 * an example, please see the reference provided in the see also section of this comment.
+	 * an example, please see the project description.
 	 * 
-	 * @see http://tinyurl.com/asvzut9
 	 * @param src1 the first BufferedImage
 	 * @param src2 the second BufferedImage
 	 * @param width the width, in pixels, of the vertical stripes.
@@ -51,23 +48,19 @@ public interface Artsy {
 	/**
 	 * Given a BufferedImage, this method returns a new BufferedImage which contains
 	 * the source image rotated clockwise by a certain number of degrees. The image
-	 * should be rotated about the center of the image.
-	 * 
+	 * should be rotated about the center of the image. For an example, please see
+     * the project description.
+     *
+     * <p>
+     * The easiest way to rotate an image is to use an affine transformation.
+     * See the reference mentioned below for a link to AWT's
+     * <code>AffineTransform</code> class.
+     * 
+	 * @see http://docs.oracle.com/javase/8/docs/api/java/awt/geom/AffineTransform.html
 	 * @param src     the source BufferedImage
 	 * @param degrees the degrees to rotate the image, in degrees (not radians)
 	 * @return a new BufferedImage containing a rotated version of the source image
 	 */
 	public BufferedImage doRotate(BufferedImage src, double degrees);
 	
-	/**
-	 * Given two BufferedImages, this method returns a java.awt.Dimension object
-	 * where the width is set to the minimum of the two width and the height is
-	 * set to the minimum of the two heights.
-	 * 
-	 * @param src1 a BufferedImage
-	 * @param src2 a BufferedImage
-	 * @return a Dimension object containing the minimum width and height.
-	 */
-	public Dimension getMinDimension(BufferedImage src1, BufferedImage src2);
-
 } // Artsy
