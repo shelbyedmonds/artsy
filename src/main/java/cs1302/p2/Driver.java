94cd17959e03225d6ee0feec26e1706642fd0bdf
shelbyedmonds <sce60786@uga.edu>
2016-03-29 21:51:34 -0400

    added the menu bar hopefully

--- src/main/java/cs1302/p2/Driver.java
+++ src/main/java/cs1302/p2/Driver.java
@@ -6,6 +6,10 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+
 
 /**
  * This is the driver for this application.
@@ -22,7 +26,23 @@ public class Driver extends Application {
         stage.setTitle("Artsy!");
         stage.sizeToScene();
         stage.show();
-        
+	
+
+	MenuBar myMenu= new MenuBar();
+	Menu fileTab= new Menu("File");
+	MenuItem openItem= new MenuItem("Open");
+	MenuItem picItem= new MenuItem("Open Image 1");
+	MenuItem pic2Item= new MenuItem("Open Image 2");
+	MenuItem saveItem= new MenuItem("Save Result As");
+	MenuItem exitItem= new MenuItem("Exit");
+	Menu openMenu= new Menu("Open");
+	openMenu.getItems().addAll(picItem, pic2Item);
+	fileTab.getItems().addAll(openMenu, new SeparatorMenuItem(), saveItem,new SeperatorMenuItem(), exitItem);
+	menuBar.getMenus().add(fileTab);
+	
+	root.setTop(menuBar);
+	stage.setScene(scene);
+	stage.show();        
     } // createAndShowGUI
 
     public static void main(String[] args) {
